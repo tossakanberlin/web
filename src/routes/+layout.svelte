f9afd001205b806256d19dd0868749e655406e26
unknown <phurin8787@gmail.com>
2023-04-27 22:51:50 +0200

    Bright First commit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script>
-  import "../app.css";
+	import '../app.css';
 </script>
+
 <a href="/">HOME</a>
 <a href="/about-us">ABOUT US</a>
-<a href="/blog">BLOG</a>
+
 <slot />
+<a href="/blog">BLOG</a>
