6c318f584155e2bfc4e9dfdade2b0d17bb4e391d
Woraphon Choaywong <phoori12@gmail.com>
2023-04-30 15:08:58 +0200

    footer responsive

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <nav class="bg-blue py-5 px-10 flex flex-row justify-between font-serif text-4xl text-white">
-	<ul class="flex-1 flex justify-start mr-auto">
+	<ul class="flex-1 justify-start mr-auto md:flex hidden">
 		<li><a href="/" class="m-6">Home</a></li>
 		<li><a href="/about-us" class="m-6">About us</a></li>
 		<li><a href="/about-us" class="m-6">Tossakan</a></li>
@@ -14,20 +14,37 @@
 	</div>
 	<div class="flex-1 flex justify-end ml-auto">
 		<h2><a href="/about-us" class="m-6">FAQ and Contact</a></h2>
-		<h2><a href="/about-us" class="m-6">Instagram Logo</a></h2>
+		<h2>
+			<a href="/about-us" class="m-6">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="24"
+					height="24"
+					viewBox="0 0 24 24"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					class="w-8 h-8 inline-block"
+					><rect width="20" height="20" x="2" y="2" rx="5" ry="5" /><path
+						d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"
+					/><line x1="17.5" x2="17.51" y1="6.5" y2="6.5" /></svg
+				>
+			</a>
+		</h2>
 	</div>
 </nav>
 
 <slot />
 
-
 <footer class="bg-blue py-5 font-serif text-white">
 	<div class="mx-6 space-y-5">
-		<div class="">
+		<div class="text-center md:text-left">
 			<p class="text-5xl italic">OUR LOCATIONS</p>
 		</div>
-		<div class="grid grid-cols-3 text-2xl">
-			<div class="flex flex-col justify-between items-start gap-4">
+		<div class="grid grid-cols-1 md:grid-cols-3 text-2xl md:gap-0 gap-4">
+			<div class="flex flex-col justify-between items-center md:items-start gap-4">
 				<div>
 					<ul class="text-center">
 						<li><a href="/">TOSSAKAN</a></li>
@@ -69,7 +86,7 @@
 				</div>
 			</div>
 
-			<div class="flex flex-col justify-between items-end">
+			<div class="flex flex-col justify-between items-center md:items-end">
 				<div class="flex flex-col justify-between h-full">
 					<div>
 						<ul class="text-center">
