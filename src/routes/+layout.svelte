c8522d42aca022a8d6484e86efcafb4954d4be06
Woraphon Choaywong <phoori12@gmail.com>
2023-04-30 19:21:52 +0200

    routing to faq

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,7 +13,7 @@
 		<h2>Tossakan</h2>
 	</div>
 	<div class="flex-1 flex justify-end ml-auto">
-		<h2><a href="/about-us" class="m-33">FAQ and Contact</a></h2>
+		<h2><a href="/job" class="m-33">FAQ and Contact</a></h2>
 		<h2>
 			<a href="/about-us" class="m-3">
 				<svg
