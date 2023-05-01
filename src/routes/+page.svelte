e6f1640417815474ab0d696adbbc9434f54d6f8b
unknown <phurin8787@gmail.com>
2023-05-01 19:47:34 +0200

    Fixing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,31 +2,32 @@
 	<div class=" absolute top-0 m-2 w-full text-center z-10">
 		<h1 class="text-5xl text-white uppercase">Neo-ThaiCuisine</h1>
 	</div>
-	<div class="grid grid-cols-2">
-		<div class="relative flex items-center justify-center group">
+	<div class="grid grid-cols-2 ">
+		<div class="  relative flex items-center justify-center group  ">
+			
 			<div
-				class=" flex-col z-10 rounded-full aspect-square bg-pink flex justify-center items-center px-4 group-hover:bg-opacity-0 transition-all duration-700 cursor-pointer"
+				class="   flex-col z-10 rounded-full h-[300px] w-[300px] aspect-square bg-pink flex justify-center items-center px-4 group-hover:bg-opacity-0 transition-all duration-700 cursor-pointer"
 			>
 				<img
 					src="/Tossakan_logo.png"
-					class=" w-[300px] mt-7 object-contain group-hover:scale-[1.2] transition-all ease-in-out duration-500"
+					class=" w-[350px] mt-7 object-contain group-hover:scale-[1.2] transition-all ease-in-out duration-500"
 				/>
 				<h2 class="text-3xl text-center text-white uppercase group-hover:scale-[1.2] transition-all ease-in-out duration-500">Tossakan<br />P-Berg</h2>
 			</div>
-			<img src="/Horizontal_left.jpg" class="absolute top-0 left-0 z-[-1]" />
+			<img src="/img1.jpeg" class="  absolute z-[-1] " />
 		</div>
 
-		<div class="relative flex items-center justify-center group">
+		<div class="relative flex items-center justify-center group ">
 			<div
-				class=" flex-col z-10 rounded-full aspect-square bg-pink flex justify-center items-center px-4 group-hover:bg-opacity-0 transition-all duration-700 cursor-pointer"
+				class=" flex-col z-10 rounded-full h-[300px] w-[300px] aspect-square bg-pink flex justify-center items-center px-4 group-hover:bg-opacity-0 transition-all duration-700 cursor-pointer"
 			>
 				<img
-					src="/Tossakan_logo.png"
-					class=" w-[300px] mt-7 object-contain group-hover:scale-[1.2] transition-all ease-in-out duration-500"
+					src="/Maiyarap_logo.png"
+					class=" w-[400px] mt-7 object-contain group-hover:scale-[1.2] transition-all ease-in-out duration-500"
 				/>
 				<h2 class="text-3xl text-center text-white uppercase group-hover:scale-[1.2] transition-all ease-in-out duration-500">Maiyarap<br />x-Berg</h2>
 			</div>
-			<img src="/Horizontal_right.jpg" class="absolute top-0 left-0 z-[-1]" />
+			<img src="/img_2.jpg" class="absolute  z-[-1]  " />
 		</div>
 	</div>
 </article>
