6a6bd72059675034c64a2ff64b0229ce1e709596
Woraphon Choaywong <phoori12@gmail.com>
2023-05-06 18:19:14 +0200

    menus

--- src/routes/maiyarap/+page.svelte
+++ src/routes/maiyarap/+page.svelte
@@ -9,140 +9,144 @@
 	let isInView_pic: boolean
 	let options = {}
 </script>
-<Menu></Menu>	
-<div class={$hamburger? "hidden":  "block"}> 
-<div class="flex flex-col justify-between items-center bg-blue text-white py-20">
-	<div
-		class="grid grid-cols-1 md:grid-cols-2 gap-10 items-center"
-		use:inview={options}
-		on:inview_enter={({ detail }) => {
-			isInView_maiyarap = detail.inView
-		}}
-	>
-		{#if isInView_maiyarap}
-			<div in:fade={{ duration: 500 }}>
-				<Image
-					width={600}
-					src="maiyarap_logo.png"
-					class="max-h-[360px] w-full object-contain top-0 left-0"
-					alt="maiyarap"
-				/>
-			</div>
-		{:else}
-			<div class="h-[360px] md:h-full" />
-		{/if}
-		<div
-			class="flex flex-col justify-between text-center md:text-start md:items-start mr-10 ml-10 font-serif"
-		>
-			<div class="grid text-xl gap-10">
-				<div
-					class="grid grid-cols-1 md:grid-cols-2 justify-between md:justify-start text-center md:text-start font-serif sm:gap-5 xl:gap-0"
-				>
-					<div class="flex flex-col col-span-full xl:col-span-1">
-						<h2 class="text-5xl sm:text-8xl">MAIYARAP</h2>
-						<h2 class="text-2xl font-sans md:-mt-5">ORANIENSTRASSE 200, 10999 BERLIN</h2>
-					</div>
-					<div
-						class="flex flex-col md:flex-row xl:flex-col col-span-full xl:col-span-1 items-center justify-center md:justify-start xl:justify-center"
-					>
-						<button
-							type="submit"
-							class=" bg-pink text-white rounded-md shadow-none font-serif text-2xl mx-5 md:mx-0 xl:mx-5 sm:mt-0 my-5 py-1 w-[15rem]"
-							>MENU
-						</button>
-					</div>
-					<div class="flex flex-col justify-center items-center" />
-				</div>
-				<div class="grid grid-cols-1 md:grid-cols-2 text-3xl gap-5 font-sans">
-					<div>
-						<h2>OPEN DAILY</h2>
-						<h2>12:00 - 22:00</h2>
-					</div>
-					<div>
-						<h2>KITCHEN CLOSE</h2>
-						<h2>21:00</h2>
-					</div>
-				</div>
-				<div
-					class="grid grid-cols-1 md:grid-cols-2 justify-between md:justify-start text-center md:text-start font-sans gap-5 md:gap-5 xl:gap-0"
-				>
-					<h2 class="text-xl col-span-full xl:col-span-1">*no reservations, just walk ins 😊</h2>
-					<div
-						class="flex flex-col md:flex-row xl:flex-col col-span-full xl:col-span-1 items-center justify-center md:justify-start xl:justify-center"
-					>
-						<button
-							type="submit"
-							class=" bg-green_light text-blue rounded-md shadow-none font-serif text-2xl mx-5 md:mx-0 xl:mx-5 sm:mt-0 place-self-center my-5 py-1 px-5 w-[15rem]"
-							>CONTACT US
-						</button>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
 
-<div
-	class="flex flex-col justify-between items-center md:items-start bg-white text-blue py-10 gap-10 mx-10 mt-10"
->
-	<div class="flex flex-col place-items-center md:place-items-start text-center md:text-start">
-		<h2 class="text-5xl">THAI NOODLE BAR</h2>
-		<div class="flex flex-col md:flex-row gap-5">
-			<h2 class="font-sans text-5xl">thai noodle soup aka</h2>
-			<h2 class="font-sans text-5xl bg-pink text-white">gieuw tieuw</h2>
-		</div>
-	</div>
-
-	<div
-		class="flex flex-col justify-between text-center md:text-start font-sans text-2xl gap-5 mb-10"
-	>
-		<p>
-			If you´ve been to Thailand, you know how much we love our noodle soups. The diverse & hearty
-			soup is sold in every corner 24/7 in Bangkok.
-		</p>
-		<p>Here at Maiyarap, our new location in Kreuzberg, we offer only the best of the best.</p>
-		<p>Gieuw Tieuw paired with our signature dumplings and unique cocktails🥂</p>
-	</div>
-	<div class="w-full">
+<Menu />
+<div class={$hamburger ? 'hidden' : 'block'}>
+	<div class="flex flex-col justify-between items-center bg-blue text-white py-20">
 		<div
-			class="grid w-full mb-5 grid-cols-1 md:grid-cols-2 gap-5"
+			class="grid grid-cols-1 md:grid-cols-2 gap-10 items-center"
 			use:inview={options}
 			on:inview_enter={({ detail }) => {
-				isInView_pic = detail.inView
+				isInView_maiyarap = detail.inView
 			}}
 		>
-			{#if isInView_pic}
-				<div in:scale={{ duration: 1500, delay: 100 }}>
+			{#if isInView_maiyarap}
+				<div in:fade={{ duration: 500 }}>
 					<Image
 						width={600}
-						src="maiyarap_pic.jpeg"
-						class="max-h-full w-full object-contain top-0 left-0"
+						src="maiyarap_logo.png"
+						class="max-h-[360px] w-full object-contain top-0 left-0"
 						alt="maiyarap"
 					/>
 				</div>
-				<div class="flex flex-col gap-5">
-					<div class="md:relative h-[50%] w-full" in:scale={{ duration: 1500, delay: 750 }}>
-						<Image
-							src="maiyarap_pic1.jpg"
-							class="md:absolute h-[100%] w-full object-cover top-0 left-0"
-							alt="maiyarap"
-						/>
+			{:else}
+				<div class="h-[360px] md:h-full" />
+			{/if}
+			<div
+				class="flex flex-col justify-between text-center md:text-start md:items-start mr-10 ml-10 font-serif"
+			>
+				<div class="grid text-xl gap-10">
+					<div
+						class="grid grid-cols-1 md:grid-cols-2 justify-between md:justify-start text-center md:text-start font-serif sm:gap-5 xl:gap-0"
+					>
+						<div class="flex flex-col col-span-full xl:col-span-1">
+							<h2 class="text-5xl sm:text-8xl">MAIYARAP</h2>
+							<h2 class="text-2xl font-sans md:-mt-5">ORANIENSTRASSE 200, 10999 BERLIN</h2>
+						</div>
+						<div
+							class="flex flex-col md:flex-row xl:flex-col col-span-full xl:col-span-1 items-center justify-center md:justify-start xl:justify-center"
+						>
+							<a href="menu_maiyarap.pdf" target="_blank">
+								<button
+									type="submit"
+									class=" bg-pink text-white rounded-md shadow-none font-serif text-2xl mx-5 md:mx-0 xl:mx-5 sm:mt-0 my-5 py-1 w-[15rem]"
+									>MENU
+								</button>
+							</a>
+						</div>
+						<div class="flex flex-col justify-center items-center" />
 					</div>
-					<div class="relative h-[50%] w-full" in:scale={{ duration: 1500, delay: 1250 }}>
-						<Image
-							src="maiyarap_pic2.jpeg"
-							class="md:absolute h-[100%] w-full object-cover top-0 left-0"
-							alt="maiyarap"
-						/>
+					<div class="grid grid-cols-1 md:grid-cols-2 text-3xl gap-5 font-sans">
+						<div>
+							<h2>OPEN DAILY</h2>
+							<h2>12:00 - 22:00</h2>
+						</div>
+						<div>
+							<h2>KITCHEN CLOSE</h2>
+							<h2>21:00</h2>
+						</div>
+					</div>
+					<div
+						class="grid grid-cols-1 md:grid-cols-2 justify-between md:justify-start text-center md:text-start font-sans gap-5 md:gap-5 xl:gap-0"
+					>
+						<h2 class="text-xl col-span-full xl:col-span-1">*no reservations, just walk ins 😊</h2>
+						<div
+							class="flex flex-col md:flex-row xl:flex-col col-span-full xl:col-span-1 items-center justify-center md:justify-start xl:justify-center"
+						>
+							<button
+								type="submit"
+								class=" bg-green_light text-blue rounded-md shadow-none font-serif text-2xl mx-5 md:mx-0 xl:mx-5 sm:mt-0 place-self-center my-5 py-1 px-5 w-[15rem]"
+								>CONTACT US
+							</button>
+						</div>
 					</div>
 				</div>
-			{:else}
-				<div class="h-[500px] md:h-[1000px]" />
-			{/if}
+			</div>
+		</div>
+	</div>
+
+	<div
+		class="flex flex-col justify-between items-center md:items-start bg-white text-blue py-10 gap-10 mx-10 mt-10"
+	>
+		<div class="flex flex-col place-items-center md:place-items-start text-center md:text-start">
+			<h2 class="text-5xl">THAI NOODLE BAR</h2>
+			<div class="flex flex-col md:flex-row gap-5">
+				<h2 class="font-sans text-5xl">thai noodle soup aka</h2>
+				<h2 class="font-sans text-5xl bg-pink text-white">gieuw tieuw</h2>
+			</div>
+		</div>
+
+		<div
+			class="flex flex-col justify-between text-center md:text-start font-sans text-2xl gap-5 mb-10"
+		>
+			<p>
+				If you´ve been to Thailand, you know how much we love our noodle soups. The diverse & hearty
+				soup is sold in every corner 24/7 in Bangkok.
+			</p>
+			<p>Here at Maiyarap, our new location in Kreuzberg, we offer only the best of the best.</p>
+			<p>Gieuw Tieuw paired with our signature dumplings and unique cocktails🥂</p>
+		</div>
+		<div class="w-full">
+			<div
+				class="grid w-full mb-5 grid-cols-1 md:grid-cols-2 gap-5"
+				use:inview={options}
+				on:inview_enter={({ detail }) => {
+					isInView_pic = detail.inView
+				}}
+			>
+				{#if isInView_pic}
+					<div in:scale={{ duration: 1500, delay: 100 }}>
+						<Image
+							width={600}
+							src="maiyarap_pic.jpeg"
+							class="max-h-full w-full object-contain top-0 left-0"
+							alt="maiyarap"
+						/>
+					</div>
+					<div class="flex flex-col gap-5">
+						<div class="md:relative h-[50%] w-full" in:scale={{ duration: 1500, delay: 750 }}>
+							<Image
+								src="maiyarap_pic1.jpg"
+								class="md:absolute h-[100%] w-full object-cover top-0 left-0"
+								alt="maiyarap"
+							/>
+						</div>
+						<div class="relative h-[50%] w-full" in:scale={{ duration: 1500, delay: 1250 }}>
+							<Image
+								src="maiyarap_pic2.jpeg"
+								class="md:absolute h-[100%] w-full object-cover top-0 left-0"
+								alt="maiyarap"
+							/>
+						</div>
+					</div>
+				{:else}
+					<div class="h-[500px] md:h-[1000px]" />
+				{/if}
+			</div>
 		</div>
 	</div>
 </div>
-</div>
+
 <style lang="postcss">
 	:global(html) {
 		background-color: theme(colors.white);
--- src/routes/tossakan/+page.svelte
+++ src/routes/tossakan/+page.svelte
@@ -10,185 +10,186 @@
 	let isInView_pic2: boolean
 	let options = {}
 </script>
-<Menu></Menu>	
-<div class={$hamburger? "hidden":  "block"}> 
 
-
-<div class="justify-between items-center bg-green_light text-pink py-10">
-	<div
-		class="grid grid-cols-1 md:grid-cols-2 gap-10 items-center"
-		use:inview={options}
-		on:inview_enter={({ detail }) => {
-			isInView_tossakan = detail.inView
-		}}
-	>
-		{#if isInView_tossakan}
-			<div in:fade={{ duration: 500 }}>
-				<Image
-					width={600}
-					src="tossakan_logo.png"
-					class="max-h-[360px] w-full object-contain top-0 left-0"
-					alt="tossakan"
-				/>
-			</div>
-		{:else}
-			<div class="h-[360px] md:h-full" />
-		{/if}
+<Menu />
+<div class={$hamburger ? 'hidden' : 'block'}>
+	<div class="justify-between items-center bg-green_light text-pink py-10">
 		<div
-			class="flex flex-col justify-between text-center md:text-start md:items-start mr-10 ml-10 font-serif"
+			class="grid grid-cols-1 md:grid-cols-2 gap-10 items-center"
+			use:inview={options}
+			on:inview_enter={({ detail }) => {
+				isInView_tossakan = detail.inView
+			}}
 		>
-			<div class="grid text-xl gap-10">
-				<div
-					class="grid grid-cols-1 md:grid-cols-2 justify-between md:justify-start text-center md:text-start font-serif gap-10 xl:gap-0"
-				>
-					<div class="flex flex-col col-span-full xl:col-span-1">
-						<h2 class="text-5xl sm:text-8xl">TOSSAKAN</h2>
-						<h2 class="text-2xl font-sans">SCLIEMANNSTRASSE 16, 10437 BERLIN</h2>
-					</div>
+			{#if isInView_tossakan}
+				<div in:fade={{ duration: 500 }}>
+					<Image
+						width={600}
+						src="tossakan_logo.png"
+						class="max-h-[360px] w-full object-contain top-0 left-0"
+						alt="tossakan"
+					/>
+				</div>
+			{:else}
+				<div class="h-[360px] md:h-full" />
+			{/if}
+			<div
+				class="flex flex-col justify-between text-center md:text-start md:items-start mr-10 ml-10 font-serif"
+			>
+				<div class="grid text-xl gap-10">
 					<div
-						class="flex flex-col md:flex-row xl:flex-col col-span-full xl:col-span-1 justify-center items-center lg:items-start xl:items-center"
+						class="grid grid-cols-1 md:grid-cols-2 justify-between md:justify-start text-center md:text-start font-serif gap-10 xl:gap-0"
 					>
-						<button
-							type="submit"
-							class=" bg-pink text-white rounded-md shadow-none font-serif text-2xl mx-5 lg:mx-0 xl:mx-5 sm:mt-0 my-5 py-1 w-[15rem]"
-							>MENU
-						</button>
-						<button
-							type="submit"
-							class=" bg-pink text-white rounded-md shadow-none font-serif text-2xl mx-5 sm:mt-0 my-5 py-1 w-[15rem]"
-							>BOOK A TABLE
-						</button>
+						<div class="flex flex-col col-span-full xl:col-span-1">
+							<h2 class="text-5xl sm:text-8xl">TOSSAKAN</h2>
+							<h2 class="text-2xl font-sans">SCLIEMANNSTRASSE 16, 10437 BERLIN</h2>
+						</div>
+						<div
+							class="flex flex-col md:flex-row xl:flex-col col-span-full xl:col-span-1 justify-center items-center lg:items-start xl:items-center"
+						>
+							<a href="menu_tossakan.pdf" target="_blank">
+								<button
+									type="submit"
+									class=" bg-pink text-white rounded-md shadow-none font-serif text-2xl mx-5 lg:mx-0 xl:mx-5 sm:mt-0 my-5 py-1 w-[15rem]"
+									>MENU
+								</button></a
+							>
+							<button
+								type="submit"
+								class=" bg-pink text-white rounded-md shadow-none font-serif text-2xl mx-5 sm:mt-0 my-5 py-1 w-[15rem]"
+								>BOOK A TABLE
+							</button>
+						</div>
 					</div>
-				</div>
-				<h2 class="text-5xl font-sans">OPEN DAILY</h2>
-				<div class="grid grid-cols-1 md:grid-cols-2 font-sans text-3xl">
-					<h2>SUNDAY - THURSDAY</h2>
-					<h2>12:00 - 22:00</h2>
-					<h2>FRIDAY - SATURDAY</h2>
-					<h2>12:00 - 22:30</h2>
-				</div>
-				<div class="grid grid-cols-1 md:grid-cols-2 font-sans text-3xl">
-					<div class="flex flex-col">
-						<h2>KITCHEN CLOSE</h2>
+					<h2 class="text-5xl font-sans">OPEN DAILY</h2>
+					<div class="grid grid-cols-1 md:grid-cols-2 font-sans text-3xl">
+						<h2>SUNDAY - THURSDAY</h2>
+						<h2>12:00 - 22:00</h2>
+						<h2>FRIDAY - SATURDAY</h2>
+						<h2>12:00 - 22:30</h2>
 					</div>
-					<div class="flex flex-col">
-						<h2>21:00</h2>
+					<div class="grid grid-cols-1 md:grid-cols-2 font-sans text-3xl">
+						<div class="flex flex-col">
+							<h2>KITCHEN CLOSE</h2>
+						</div>
+						<div class="flex flex-col">
+							<h2>21:00</h2>
+						</div>
 					</div>
-				</div>
-				<div class="grid grid-cols-1 md:grid-cols-2 font-sans text-3xl gap-5 xl:gap-0">
-					<div class="flex flex-col col-span-full xl:col-span-1">
-						<h2 class="text-3xl">TEL: +49 30 81477638</h2>
-					</div>
-					<div
-						class="flex flex-col mt-10 md:mt-0 place-self-center md:place-self-start xl:place-self-center"
-					>
-						<button
-							type="submit"
-							class=" bg-blue_light text-white rounded-md shadow-none font-serif text-2xl xl:mx-5 sm:mt-0 place-self-center my-5 py-1 w-[15rem]"
-							>CONTACT US
-						</button>
+					<div class="grid grid-cols-1 md:grid-cols-2 font-sans text-3xl gap-5 xl:gap-0">
+						<div class="flex flex-col col-span-full xl:col-span-1">
+							<h2 class="text-3xl">TEL: +49 30 81477638</h2>
+						</div>
+						<div
+							class="flex flex-col mt-10 md:mt-0 place-self-center md:place-self-start xl:place-self-center"
+						>
+							<button
+								type="submit"
+								class=" bg-blue_light text-white rounded-md shadow-none font-serif text-2xl xl:mx-5 sm:mt-0 place-self-center my-5 py-1 w-[15rem]"
+								>CONTACT US
+							</button>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-</div>
 
-<div class="grid justify-center items-center bg-white py-10">
-	<div class="flex m-10">
-		<div class="grid grid-cols-1 md:grid-cols-2 gap-5">
-			<div
-				class="text-blue flex flex-col gap-[5rem] text-center md:text-start items-center md:items-start"
-			>
-				<div class="grid gap-3">
-					<div class="font-serif text-5xl">
-						<h2>TABLE RESERVATION 🍷</h2>
-					</div>
-					<ul class="font-sans md:ml-10 text-xl">
-						<li>9 mins for groups up to 3</li>
-						<li>120 mins for groups up to 4</li>
-					</ul>
-				</div>
-				<div class="grid gap-3">
-					<div class="font-serif text-5xl">
-						<h2>PLEASE ARRIVE ON TIME ⌚</h2>
-					</div>
-					<ul class="font-sans md:ml-10 text-xl">
-						<li>Tables are held max. 15 min</li>
-						<li>After the grace period, tables will be given to waiting guests</li>
-					</ul>
-				</div>
-				<div class="grid gap-3">
-					<div class="font-serif text-5xl">
-						<h2>please keep in mind… 💟</h2>
-					</div>
-					<ul class="font-sans md:ml-10 text-xl">
-						<li>we are a small business with very limited space.</li>
-						<li>If there are any changes to your</li>
-						<li class="flex flex-row gap-3">
-							reservation, <p class="font-bold">please call us</p>
-						</li>
-					</ul>
-				</div>
-			</div>
-
-			<div
-				class="grid gap-10 text-center md:text-start"
-				use:inview={options}
-				on:inview_enter={({ detail }) => {
-					isInView_pic1 = detail.inView
-				}}
-			>
-				<div class="flex flex-col text-blue mt-[5rem] md:mt-0">
+	<div class="grid justify-center items-center bg-white py-10">
+		<div class="flex m-10">
+			<div class="grid grid-cols-1 md:grid-cols-2 gap-5">
+				<div
+					class="text-blue flex flex-col gap-[5rem] text-center md:text-start items-center md:items-start"
+				>
 					<div class="grid gap-3">
 						<div class="font-serif text-5xl">
-							<h2>Special Events & Private Parties</h2>
+							<h2>TABLE RESERVATION 🍷</h2>
 						</div>
 						<ul class="font-sans md:ml-10 text-xl">
-							<li>We offer catering service on-site as well as off-site.</li>
-							<li>
-								From 15 up to 500 guests, our caterings are offered for private functions, corporate
-								and cultural events.
+							<li>9 mins for groups up to 3</li>
+							<li>120 mins for groups up to 4</li>
+						</ul>
+					</div>
+					<div class="grid gap-3">
+						<div class="font-serif text-5xl">
+							<h2>PLEASE ARRIVE ON TIME ⌚</h2>
+						</div>
+						<ul class="font-sans md:ml-10 text-xl">
+							<li>Tables are held max. 15 min</li>
+							<li>After the grace period, tables will be given to waiting guests</li>
+						</ul>
+					</div>
+					<div class="grid gap-3">
+						<div class="font-serif text-5xl">
+							<h2>please keep in mind… 💟</h2>
+						</div>
+						<ul class="font-sans md:ml-10 text-xl">
+							<li>we are a small business with very limited space.</li>
+							<li>If there are any changes to your</li>
+							<li class="flex flex-row gap-3">
+								reservation, <p class="font-bold">please call us</p>
 							</li>
-							<li>Please E-mail us for further information.</li>
-							<li class="mt-5">👉info@tossakan-berlin.de</li>
 						</ul>
 					</div>
 				</div>
-				{#if isInView_pic1}
-					<div in:scale={{ duration: 1500, delay: 100 }} out:fade>
-						<Image
-							width={600}
-							src="tossakan_prev_1.jpeg"
-							class="max-h-[360px] w-full object-cover top-0 left-0"
-							alt="tossakan_drinks"
-						/>
-					</div>
-				{/if}
-			</div>
 
-			<div
-				class="col-span-full mt-10"
-				use:inview={options}
-				on:inview_enter={({ detail }) => {
-					isInView_pic2 = detail.inView
-				}}
-			>
-				{#if isInView_pic2}
-					<div class="max-h-[540px] w-full" in:scale={{ duration: 1500, delay: 500 }} out:fade>
-						<Image
-							src="tossakan_prev_2.jpeg"
-							class="max-h-[540px] w-full object-cover top-0 left-0"
-							alt="tossakan_foods"
-						/>
+				<div
+					class="grid gap-10 text-center md:text-start"
+					use:inview={options}
+					on:inview_enter={({ detail }) => {
+						isInView_pic1 = detail.inView
+					}}
+				>
+					<div class="flex flex-col text-blue mt-[5rem] md:mt-0">
+						<div class="grid gap-3">
+							<div class="font-serif text-5xl">
+								<h2>Special Events & Private Parties</h2>
+							</div>
+							<ul class="font-sans md:ml-10 text-xl">
+								<li>We offer catering service on-site as well as off-site.</li>
+								<li>
+									From 15 up to 500 guests, our caterings are offered for private functions,
+									corporate and cultural events.
+								</li>
+								<li>Please E-mail us for further information.</li>
+								<li class="mt-5">👉info@tossakan-berlin.de</li>
+							</ul>
+						</div>
 					</div>
-				{:else}
-					<div class="h-[500px] md:h-[1000px]" />
-				{/if}
+					{#if isInView_pic1}
+						<div in:scale={{ duration: 1500, delay: 100 }} out:fade>
+							<Image
+								width={600}
+								src="tossakan_prev_1.jpeg"
+								class="max-h-[360px] w-full object-cover top-0 left-0"
+								alt="tossakan_drinks"
+							/>
+						</div>
+					{/if}
+				</div>
+
+				<div
+					class="col-span-full mt-10"
+					use:inview={options}
+					on:inview_enter={({ detail }) => {
+						isInView_pic2 = detail.inView
+					}}
+				>
+					{#if isInView_pic2}
+						<div class="max-h-[540px] w-full" in:scale={{ duration: 1500, delay: 500 }} out:fade>
+							<Image
+								src="tossakan_prev_2.jpeg"
+								class="max-h-[540px] w-full object-cover top-0 left-0"
+								alt="tossakan_foods"
+							/>
+						</div>
+					{:else}
+						<div class="h-[500px] md:h-[1000px]" />
+					{/if}
+				</div>
 			</div>
 		</div>
 	</div>
-</div>
 </div>
 
 <style lang="postcss">
