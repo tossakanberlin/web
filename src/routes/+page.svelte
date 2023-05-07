f5534c4b77a038ad8fdf12ee6af46750de54a593
unknown <phurin8787@gmail.com>
2023-05-07 13:50:06 +0200

    Updateing Landingpage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,7 +10,7 @@
 <Menu></Menu>	
 <div class={$hamburger ? 'hidden' : 'block'}>
 	<div class="left-0 absolute top-0  w-full text-center z-10">
-		<h1 class="text-7xl text-white uppercase my-5 md:hidden">Neo-Thai Cuisine</h1>
+		<h1 class="text-7xl text-white uppercase my-5 md:hidden" >Neo-Thai Cuisine</h1>
 	</div>
 	<div class="grid grid-cols-1 md:grid-cols-2">
 		<div class="relative flex items-center justify-center group    ">
@@ -20,11 +20,11 @@
 				<Image
 					src="/tossakan_logo.png"
 					width={500}
-					class=" lg:w-[500px] w-[150px] md:w-[200px] mt-7 max-h-screen object-contain group-hover:scale-[1.2] transition-all ease-in-out duration-500  "
+					class=" lg:w-[500px] w-[150px] md:w-[200px] mt-7 max-h-screen object-contain group-hover:scale-[1.2] transition-all ease-in-out duration-500 p-4 "
 					important
 				/>
 				<h2
-					class="lg:text-5xl lg:m-5 text-center text-white uppercase group-hover:scale-[1.2] transition-all ease-in-out duration-500 m-3 "
+					class="lg:text-5xl lg:m-5 text-center text-black uppercase group-hover:scale-[1.2] transition-all ease-in-out duration-500 m-3 "
 				>
 					Tossakan<br />P-Berg
 				</h2>
@@ -34,7 +34,7 @@
 				eager
 				important
 				src="/vertical_left.jpeg"
-				class="z-[-1] w-full h-[calc(100vh-130px)] object-cover"
+				class="z-[-1] w-full h-[calc(100vh-130px)] object-cover  transition-all ease-in-out duration-500 group-hover:opacity-50"
 			/>
 		</div>
 
@@ -45,18 +45,18 @@
 				<Image
 					width={500}
 					src="/maiyarap_logo.png"
-					class=" lg:w-[500px]  w-[150px] md:w-[200px]  max-h-screen mt-7 object-contain group-hover:scale-[1.2] transition-all ease-in-out duration-500"
+					class=" lg:w-[500px]  w-[150px] md:w-[200px]  max-h-screen mt-7 object-contain group-hover:scale-[1.2] transition-all ease-in-out duration-500 p-4"
 					important
 				/>
 				<h2
-					class="lg:text-5xl lg:m-5 text-center text-white uppercase group-hover:scale-[1.2] transition-all ease-in-out duration-500 m-3 "
+					class="lg:text-5xl lg:m-5 text-center text-black uppercase group-hover:scale-[1.2] transition-all ease-in-out duration-500 m-2 "
 				>
 					Maiyarap<br />x-Berg
 				</h2>
 			</div>
 			<Image
 				src="/vertical_right.jpeg"
-				class="  z-[-1] object-cover w-full h-[calc(100vh-130px)]"
+				class="  z-[-1] object-cover w-full h-[calc(100vh-130px)] transition-all ease-in-out duration-500 group-hover:opacity-50"
 				prioritize
 				eager
 				important
