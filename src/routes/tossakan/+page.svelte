6294b9bcac613e07942424e31ef4a4f975ea7ae7
Woraphon Choaywong <phoori12@gmail.com>
2023-05-12 18:18:19 +0200

    tossakan page fix (client's request)

--- src/routes/tossakan/+page.svelte
+++ src/routes/tossakan/+page.svelte
@@ -4,36 +4,25 @@
 	import { fade, scale } from 'svelte/transition'
 	import Menu from '$lib/components/Overlay_menu.svelte'
 
-	let isStuck = false
-	// let widget_class =
-	// 	' bg-pink rounded-full shadow-sm text-white px-5 py-5 self-end       hover:scale-[1.1]  transition-all ease-in-out duration-500'
-	// let active = true
-
-	// function handleStuck(e) {
-	// 	isStuck = e.detail.isStuck
-	// }
-
 	let isInView_tossakan: boolean
 	let isInView_pic1: boolean
 	let isInView_pic2: boolean
 	let options = {}
-	// let stickToTop = false
 </script>
 
 <Menu />
 
-
 <div>
 	<div class="justify-between items-center bg-green_light text-pink py-10">
 		<div
-			class="grid grid-cols-1 md:grid-cols-2 gap-10 items-center"
+			class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 items-center mx-5"
 			use:inview={options}
 			on:inview_enter={({ detail }) => {
 				isInView_tossakan = detail.inView
 			}}
 		>
 			{#if isInView_tossakan}
-				<div in:fade={{ duration: 500 }}>
+				<div class="flex md:max-w-[450px] max-h-[270px]" in:fade={{ duration: 500 }}>
 					<Image
 						width={600}
 						height={360}
@@ -46,55 +35,52 @@
 				<div class="h-[360px] md:h-full" />
 			{/if}
 			<div
-				class="flex flex-col justify-between text-center md:text-start md:items-start mr-10 ml-10 font-serif"
+				class="flex flex-col justify-between text-center md:text-start md:items-start ml-10 font-serif lg:col-span-2"
 			>
-				<div class="grid text-xl gap-10">
+				<div class="grid text-xl gap-5 w-full">
 					<div
 						class="grid grid-cols-1 md:grid-cols-2 justify-between md:justify-start text-center md:text-start font-serif gap-10 xl:gap-0"
 					>
-						<div class="flex flex-col col-span-full xl:col-span-1">
+						<div class="flex flex-col col-span-1 md:col-span-2 lg:col-span-1">
 							<h2 class="text-5xl sm:text-8xl">TOSSAKAN</h2>
-							<h2 class="text-2xl font-sans">SCLIEMANNSTRASSE 16, 10437 BERLIN</h2>
+							<h2 class="text-2xl font-sans">SCLIEMANNSTRASSE 16</h2>
+							<h2 class="text-2xl font-sans md:-mt-2">10437 BERLIN</h2>
 						</div>
-						<div
-							class="flex flex-col md:flex-row xl:flex-col col-span-full xl:col-span-1 justify-center items-center md:items-start lg:items-start xl:items-center"
-						>
+						<div class="flex flex-col md:flex-row lg:flex-col col-span-full lg:col-span-1 justify-center md:justify-start lg:justify-center items-center md:items-start lg:items-center gap-0 md:gap-10 lg:gap-0">
 							<a href="menu_tossakan.pdf" target="_blank">
 								<button
 									type="submit"
-									class=" bg-pink hover:bg-[#ff64b9] text-white rounded-md shadow-none font-serif text-2xl mx-5 lg:mx-0 xl:mx-5 sm:mt-0 my-5 md:my-0 xl:my-5 py-1 w-[15rem] md:w-[12rem] xl:w-[15rem]"
+									class=" bg-pink hover:bg-[#ff64b9] text-white rounded-md shadow-none font-serif text-2xl mx-5 md:mx-0 lg:mx-5 sm:mt-0 my-5 md:my-0 lg:my-5 py-1 w-[15rem] md:w-[12rem] xl:w-[15rem]"
 									>MENU
 								</button></a
 							>
 							{@html `<div class="quandoo-widget-builder" data-config='{"format":"text-button","bgcolor":"#1870C3","txcolor":"#ffffff","round":"yes","position":"","font":"md","merchant":97442,"txt":"Book a Table"}'></div>`}
 						</div>
 					</div>
-					<h2 class="text-5xl font-sans">OPEN DAILY</h2>
-					<div class="grid grid-cols-1 md:grid-cols-2 font-sans text-3xl">
+					<h2 class="text-4xl font-sans">OPEN DAILY</h2>
+					<div class="grid grid-cols-1 md:grid-cols-2 font-sans text-3xl md:text-2xl lg:text-3xl">
 						<h2>SUNDAY - THURSDAY</h2>
 						<h2>12:00 - 22:00</h2>
 						<h2>FRIDAY - SATURDAY</h2>
 						<h2>12:00 - 22:30</h2>
 					</div>
-					<div class="grid grid-cols-1 md:grid-cols-2 font-sans text-3xl">
+					<div class="grid grid-cols-1 md:grid-cols-2 font-sans text-3xl md:text-2xl lg:text-3xl">
 						<div class="flex flex-col">
 							<h2>KITCHEN CLOSE</h2>
 						</div>
 						<div class="flex flex-col">
-							<h2>21:00</h2>
+							<h2>21:30</h2>
 						</div>
 					</div>
-					<div class="grid grid-cols-1 md:grid-cols-2 font-sans text-3xl gap-5 xl:gap-0">
-						<div class="flex flex-col col-span-full xl:col-span-1">
+					<div class="grid grid-cols-1 md:grid-cols-2 font-sans text-3xl">
+						<div class="flex flex-col col-span-full lg:col-span-1">
 							<h2 class="text-3xl">TEL: +49 30 81477638</h2>
 						</div>
-						<div
-							class="flex flex-col mt-10 md:mt-0 place-self-center md:place-self-start xl:place-self-center"
-						>
+						<div class="flex flex-col mt-10 md:mt-0 place-self-center md:place-self-start lg:place-self-center">
 							<a href="/faq?tossakan">
 								<button
 									type="submit"
-									class=" bg-blue_light hover:bg-[#878bef] text-white rounded-md shadow-none font-serif text-2xl xl:mx-5 sm:mt-0 place-self-center my-5 py-1 w-[15rem]"
+									class=" bg-blue_light hover:bg-[#878bef] text-white rounded-md shadow-none font-serif text-2xl lg:mx-5 sm:mt-0 place-self-center my-5 py-1 w-[15rem] md:w-[12rem] xl:w-[15rem]"
 									>CONTACT US
 								</button>
 							</a>
@@ -112,25 +98,26 @@
 					class="text-blue flex flex-col gap-[5rem] text-center md:text-start items-center md:items-start"
 				>
 					<div class="grid gap-3">
-						<div class="font-serif text-5xl">
+						<div class="font-serif text-4xl">
 							<h2>TABLE RESERVATION 🍷</h2>
 						</div>
 						<ul class="font-sans md:ml-10 text-xl">
-							<li>9 mins for groups up to 3</li>
+							<li>90 mins for groups up to 3</li>
 							<li>120 mins for groups up to 4</li>
 						</ul>
 					</div>
 					<div class="grid gap-3">
-						<div class="font-serif text-5xl">
+						<div class="font-serif text-4xl">
 							<h2>PLEASE ARRIVE ON TIME ⌚</h2>
 						</div>
 						<ul class="font-sans md:ml-10 text-xl">
 							<li>Tables are held max. 15 min</li>
-							<li>After the grace period, tables will be given to waiting guests</li>
+							<li>After the grace period,</li>
+							<li>tables will be given to waiting guests</li>
 						</ul>
 					</div>
 					<div class="grid gap-3">
-						<div class="font-serif text-5xl">
+						<div class="font-serif text-4xl">
 							<h2>please keep in mind… 💟</h2>
 						</div>
 						<ul class="font-sans md:ml-10 text-xl">
@@ -152,7 +139,7 @@
 				>
 					<div class="flex flex-col text-blue mt-[5rem] md:mt-0">
 						<div class="grid gap-3">
-							<div class="font-serif text-5xl">
+							<div class="font-serif text-4xl">
 								<h2>Special Events & Private Parties</h2>
 							</div>
 							<ul class="font-sans md:ml-10 text-xl">
@@ -187,7 +174,10 @@
 					}}
 				>
 					{#if isInView_pic2}
-						<div class="hidden lg:block max-h-[540px] w-full" in:scale={{ duration: 1500, delay: 300 }}>
+						<div
+							class="hidden lg:block max-h-[540px] w-full"
+							in:scale={{ duration: 1500, delay: 300 }}
+						>
 							<Image
 								width={600}
 								height={540}
@@ -196,7 +186,10 @@
 								alt="tossakan/tossakan_foods"
 							/>
 						</div>
-						<div class="block lg:hidden max-h-[540px] w-full" in:scale={{ duration: 1500, delay: 300 }}>
+						<div
+							class="block lg:hidden max-h-[540px] w-full"
+							in:scale={{ duration: 1500, delay: 300 }}
+						>
 							<Image
 								width={600}
 								height={540}
@@ -214,55 +207,14 @@
 	</div>
 </div>
 
-<!-- <div
-	class="sticky mb-[20px] mr-[20px] flex flex-col justify-end"
-	class:isStuck
-	data-position={stickToTop ? 'top' : 'bottom'}
-	use:sticky={{ stickToTop }}
-	on:stuck={handleStuck}
->
-	<div id="quandoo-booking-widget" class={active ? 'hidden' : 'block'} />
-
-	<button class={widget_class} on:click={() => (active = !active)}>
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			width="36"
-			height="36"
-			viewBox="0 0 24 24"
-			fill="none"
-			stroke="currentColor"
-			stroke-width="2"
-			stroke-linecap="round"
-			stroke-linejoin="round"
-			class="lucide lucide-book"
-			><path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20" /><path
-				d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z"
-			/></svg
-		>
-	</button>
-</div> -->
-
 <svelte:head>
 	<script
 		src="https://s3-eu-west-1.amazonaws.com/quandoo-website/widget-builder/quandoo-widget-builder.js"
 	></script>
-	<!-- <script
-		src="https://booking-widget.quandoo.com/index.js"
-		data-merchant-id="97442"
-		data-theme="light"
-		data-primary-color="1870C3"
-	></script> -->
 </svelte:head>
 
 <style lang="postcss">
 	:global(html) {
 		background-color: theme(colors.white);
 	}
-	/* .sticky[data-position='top'] {
-		top: 1rem;
-	}
-
-	.sticky[data-position='bottom'] {
-		bottom: 1rem;
-	} */
 </style>
